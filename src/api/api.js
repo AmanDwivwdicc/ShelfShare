82ff45e96e07becd6d5c47c12051fae2514a76d2
AmanDwivwdicc <dwivediaman104@gmail.com>
2026-07-02 12:12:08 +0530

    some changes in api

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "/api";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "https://shelfshare-apjq.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
