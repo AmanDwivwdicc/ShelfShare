7eaec2e71da9314402f6dffb0039195c4b89f8ae
AmanDwivwdicc <dwivediaman104@gmail.com>
2026-07-16 09:49:15 +0530

    smtp used

--- backend/utils/sendEmailNodemailer.js
+++ backend/utils/sendEmailNodemailer.js
@@ -1,13 +1,43 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
+  family: 4, // Force IPv4
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
+
+export const sendEmailNodemailer = async ({
+  to,
+  subject,
+  html,
+}) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"ShelfShare" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", info.response);
+    return true;
+
+  } catch (err) {
+    console.error("Nodemailer Error:", err.message);
+    return false;
+  }
+};
 
 export const sendEmailNodemailer = async ({
   to,
