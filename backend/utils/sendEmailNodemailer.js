148ef9fe50fd0a16454cb522d48bdbf23daff90f
AmanDwivwdicc <dwivediaman104@gmail.com>
2026-07-16 09:52:50 +0530

    errors fixed

--- backend/utils/sendEmailNodemailer.js
+++ backend/utils/sendEmailNodemailer.js
@@ -39,28 +39,3 @@ export const sendEmailNodemailer = async ({
   }
 };
 
-export const sendEmailNodemailer = async ({
-  to,
-  subject,
-  html,
-}) => {
-  try {
-    const info = await transporter.sendMail({
-      from: `"ShelfShare" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("Email sent:", info.response);
-
-    return true;
-
-  } catch (err) {
-
-    console.error("Nodemailer Error:", err.message);
-
-    return false;
-
-  }
-};
